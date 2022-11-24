bab17d71d0bedd8cd64d45a3021577b333352acd
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-25 00:04:53 +0100

    Beispiel 03 unfinished

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,81 +3,94 @@ package com.cc.java;
 public class App {
     
     public static void main( String[] args ) {
-
         /************************************************************/
         /*******           Beispiel 01 laut Aufgabe           *******/
         /************************************************************/
-            Employee employee1 = new Employee( "Mey", "Carl", "Employee", 2015  );
-                output( 
-                    employee1.getInfo( "#eFN" ) + " " + employee1.getInfo( "#eLN" ) + 
-                    " (" + employee1.getInfo( "#eEY" ) + 
-                    ") --> " + employee1.getInfo( "#eRL" ) 
-                );
-                output( " " );
+            output( c( "#y" ) + "---" + c( "#r" ) + "Example 1" + c( "#y" ) + "---------------------" );
 
-            Employee employee2 = new Employee( "Doe", "John", "Leader", 2000  );
-                output( 
-                    employee2.getInfo( "#eFN" ) + " " + employee2.getInfo( "#eLN" ) + 
-                    " (" + employee2.getInfo( "#eEY" ) + 
-                    ") --> " + employee2.getInfo( "#eRL" ) 
-                );
-                output( " " );
+            Employee e1 = new Employee( "Boe", "Jane", "Employee", "2005"  );
+                output( "\t" + c( "#c" ) + e1.getInfo( "#eFN" ) + " " + e1.getInfo( "#eLN" )  + c( "#b" ) + " (" + c( "#p" ) + e1.getInfo( "#eEY" ) + c( "#b" ) + ")" +c( "#b" ) + " --> " + c( "#g" ) + e1.getInfo( "#eRL" ) );
 
-            Employee employee3 = new Employee( "Poppinks", "Marrie", "Toiletman", 2020  );
+            Employee e2 = new Employee( "Doe", "John", "Management", "2007"  );
+                output( "\t" + c( "#c" ) + e2.getInfo( "#eFN" ) + " " + e2.getInfo( "#eLN" )  + c( "#b" ) + " (" + c( "#p" ) + e2.getInfo( "#eEY" ) + c( "#b" ) + ")" +c( "#b" ) + " --> " + c( "#g" ) + e2.getInfo( "#eRL" ) );
+
+            Employee e3 = new Employee( "Foe", "Jack", "Service", "2009"  );
                 output( 
-                    employee3.getInfo( "#eFN" ) + " " + employee3.getInfo( "#eLN" ) + 
-                    " (" + employee3.getInfo( "#eEY" ) + 
-                    ") --> " + employee3.getInfo( "#eRL" ) 
+                    "\t" + 
+                    c( "#c" ) + e3.getInfo( "#eFN" ) + " " + e3.getInfo( "#eLN" )  + 
+                    c( "#b" ) + " (" + c( "#p" ) + e3.getInfo( "#eEY" ) + c( "#b" ) + ")" +
+                    c( "#b" ) + " --> " + 
+                    c( "#g" ) + e3.getInfo( "#eRL" ) 
                 );
-            output( "------------------------" );
-            
+
         /************************************************************/
         /*******       Beispiel 02 Arrays und Schleifen       *******/
         /************************************************************/
-            String[] employees[] = {
-                { "Mey", "Carl", "Employee", "2015" },
-                { "Doe", "John", "Leader", "2000" },
-                { "Poppinks", "Marrie", "Toiletman", "2020" }
+            output( "\n" + c( "#y" ) + "---" + c( "#r" ) + "Example 2" + c( "#y" ) + "---------------------" );
+            
+            String[] ea2[] = {
+                { "Goe", "Jade", "Employee", "2011" },
+                { "Hoe", "Jaco", "Engineering", "2013" },
+                { "Koe", "Jace", "Service", "2015" }
             };
 
-            Employee[] employee;
-            employee = new Employee[ employees.length ];
+            Employee[] ep2;
+            ep2 = new Employee[ ea2.length ];
 
-            for ( int i = 0; i <= employees.length -1 ; i++ ) {
-                for ( int j = 0; j <= employees[ i ].length -1 ; j++ ) {
-                    employee[ i ] = new Employee( employees[ i ][ 0 ], employees[ i ][ 1 ], employees[ i ][ 2 ], employees[ i ][ 3 ] );
+            for ( int i = 0; i <= ea2.length - 1; i++ ) {
+                for ( int j = 0; j <= ea2[ i ].length - 1; j++ ) {
+                    ep2[ i ] = new Employee( ea2[ i ][ 0 ], ea2[ i ][ 1 ], ea2[ i ][ 2 ], ea2[ i ][ 3 ] );
                 }
+                output( "\t" + c( "#c" ) + ep2[ i ].getInfo( "#eFN" ) + " " + ep2[ i ].getInfo( "#eLN" ) + c( "#b" ) + " (" + c( "#p" ) + ep2[ i ].getInfo( "#eEY" ) + c( "#b" ) + ")" +c( "#b" ) + " --> " + c( "#g" ) + ep2[ i ].getInfo( "#eRL" ) );
             }
 
+        /************************************************************/
+        /*******   Beispiel 03 voll automatisiert per Array   *******/
+        /************************************************************/
+            output( "\n" + c( "#y" ) + "---" + c( "#r" ) + "Example 3" + c( "#y" ) + "---------------------" );
             
-                output( 
-                    employee1.getInfo( "#eFN" ) + " " + employee1.getInfo( "#eLN" ) + 
-                    " (" + employee1.getInfo( "#eEY" ) + 
-                    ") --> " + employee1.getInfo( "#eRL" ) 
-                );
-                output( "------------------------" );
+            String[] ea3[] = {
+                { "Moe", "Jude", "Employee", "2017" },
+                { "Noe", "Joel", "Employee", "2019" },
+                { "Poe", "Juce", "Employee", "2021" }
+            };
+            
+            Employee[] ep3;
+            ep3 = new Employee[ ea3.length ];
 
-            Employee employee2 = new Employee( "Doe", "John", "Leader", 2000  );
-                output( 
-                    employee2.getInfo( "#eFN" ) + " " + employee2.getInfo( "#eLN" ) + 
-                    " (" + employee2.getInfo( "#eEY" ) + 
-                    ") --> " + employee2.getInfo( "#eRL" ) 
-                );
-                output( "------------------------" );
+            for ( int i = 0; i <= ea3.length - 1; i++ ) {
+                for ( int j = 0; j <= ea3[ i ].length - 1; j++ ) {
+                    ep3[ i ] = new Employee( ea3[ i ][ 0 ], ea3[ i ][ 1 ], ea3[ i ][ 2 ], ea3[ i ][ 3 ] );
+                }
+                output( "\t" + c( "#c" ) + ep3[ i ].getInfo( "#eFN" ) + " " + ep3[ i ].getInfo( "#eLN" ) + c( "#b" ) + " (" + c( "#p" ) + ep3[ i ].getInfo( "#eEY" ) + c( "#b" ) + ")" +c( "#b" ) + " --> " + c( "#g" ) + ep3[ i ].getInfo( "#eRL" ) );
+            }
+    
+        /************************************************************/
+        /*******                     Ende                     *******/
+        /************************************************************/
+            output( "\n" + c( "#y" ) + "---" + c( "#r" ) + "Ende" + c( "#y" ) + "--------------------------" );
+    }
 
-            Employee employee3 = new Employee( "Poppinks", "Marrie", "Toiletman", 2020  );
-                output( 
-                    employee3.getInfo( "#eFN" ) + " " + employee3.getInfo( "#eLN" ) + 
-                    " (" + employee3.getInfo( "#eEY" ) + 
-                    ") --> " + employee3.getInfo( "#eRL" ) 
-                );
-                output( "------------------------" );
+    /************************************************************/
+    /*******                global methods                *******/
+    /************************************************************/
+    public static void output( String o ) { System.out.println( o ); }
+    public static String c( String c ) {
+        switch ( c ) {
+            case "#r":
+                return "\u001b[31m";
+            case "#g":
+                return "\u001b[32m";
+            case "#y":
+                return "\u001b[33m";
+            case "#b":
+                return "\u001b[34m";
+            case "#p":
+                return "\u001b[35m";
+            case "#c":
+                return "\u001b[36m";
+            default:
+                return "\u001b[0m";
+        }
+    }
 }
-
-
-    public static void output( String outputStr ) { System.out.println( outputStr ); }
-
-
-
-}
-
--- com/cc/java/Employee.java
+++ com/cc/java/Employee.java
@@ -2,30 +2,43 @@ package com.cc.java;
 
 public class Employee {
 
-    private String employeeLastName;
-    private String employeeFastName;
-    private String employeeRole;
-    private int employeeEntryYear;
+    private String eLN, eFN, eRL, eEY;
 
-    public Employee(String employeeLastName, String employeeFastName, String employeeRole, int employeeEntryYear) {
-        this.employeeLastName = employeeLastName;
-        this.employeeFastName = employeeFastName;
-        this.employeeRole = employeeRole;
-        this.employeeEntryYear = employeeEntryYear;
+    public Employee( String eLN, String eFN, String eRL, String eEY ) {
+        this.eLN = eLN;
+        this.eFN = eFN;
+        this.eRL = eRL;
+        this.eEY = eEY;
     }
 
-    public String getInfo( String flag ) {
-        switch ( flag ) {
+    public void seteLN(String eLN) { this.eLN = eLN; }
+    public void seteFN(String eFN) { this.eFN = eFN; }
+    public void seteRL(String eRL) { this.eRL = eRL; }
+    public void seteEY(String eEY) { this.eEY = eEY; }
+
+    public String getInfo( String gI ) {
+        switch ( gI ) {
             case "#eLN":
-                return employeeLastName;
+                return eLN;
             case "#eFN":
-                return employeeFastName;
+                return eFN;
             case "#eEY":
-                return String.valueOf( employeeEntryYear );
+                return eEY;
             case "#eRL":
-                return employeeRole;
+                return eRL;
             default:
                 return "no correct input info";
         }
-    }    
+    }
+    public String setInfo( int sI1, String sI2 ) {
+        switch ( sI1 ) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+                return sI2;
+            default:
+                return sI2;
+        }
+    }
 }
