e634651563b36516811bf38e99325ad75553b738
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-24 14:02:14 +0100

    unfinished try of example 2

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -13,7 +13,7 @@ public class App {
                     " (" + employee1.getInfo( "#eEY" ) + 
                     ") --> " + employee1.getInfo( "#eRL" ) 
                 );
-                output( "------------------------" );
+                output( " " );
 
             Employee employee2 = new Employee( "Doe", "John", "Leader", 2000  );
                 output( 
@@ -21,7 +21,7 @@ public class App {
                     " (" + employee2.getInfo( "#eEY" ) + 
                     ") --> " + employee2.getInfo( "#eRL" ) 
                 );
-                output( "------------------------" );
+                output( " " );
 
             Employee employee3 = new Employee( "Poppinks", "Marrie", "Toiletman", 2020  );
                 output( 
@@ -29,11 +29,27 @@ public class App {
                     " (" + employee3.getInfo( "#eEY" ) + 
                     ") --> " + employee3.getInfo( "#eRL" ) 
                 );
-                output( "------------------------" );
+            output( "------------------------" );
+            
         /************************************************************/
-        /*******           Beispiel 01 laut Aufgabe           *******/
+        /*******       Beispiel 02 Arrays und Schleifen       *******/
         /************************************************************/
-            Employee employee1 = new Employee( "Mey", "Carl", "Employee", 2015  );
+            String[] employees[] = {
+                { "Mey", "Carl", "Employee", "2015" },
+                { "Doe", "John", "Leader", "2000" },
+                { "Poppinks", "Marrie", "Toiletman", "2020" }
+            };
+
+            Employee[] employee;
+            employee = new Employee[ employees.length ];
+
+            for ( int i = 0; i <= employees.length -1 ; i++ ) {
+                for ( int j = 0; j <= employees[ i ].length -1 ; j++ ) {
+                    employee[ i ] = new Employee( employees[ i ][ 0 ], employees[ i ][ 1 ], employees[ i ][ 2 ], employees[ i ][ 3 ] );
+                }
+            }
+
+            
                 output( 
                     employee1.getInfo( "#eFN" ) + " " + employee1.getInfo( "#eLN" ) + 
                     " (" + employee1.getInfo( "#eEY" ) + 
